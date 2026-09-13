1ad5375615c0d16ba7fd5991b4c3df6559a5bfc4
Arthur Cruz <arthurcruzdbp@gmail.com>
2026-09-13 03:18:10 -0300

    first

---
+++ src/AutoHome.Api/Endpoints/AuthController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Security.Claims;
+
+namespace AutoHome.Api.Endpoints;
+
+[ApiController]
+[Route("auth")]
+public class AuthController : BaseController
+{
+    
+    // [EnableRateLimiting("login")]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        
+        return Ok();
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserRequest request)
+    {
+        
+        return Ok();
+    }
+
+    [Authorize]
+    [HttpGet("me")]
+    public IActionResult Me()
+    {
+        var name = User.FindFirstValue(ClaimTypes.Name);
+        var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        return Ok(new { id, name });
+    }
+
+    [Authorize]
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        var isHttps = Request.IsHttps;
+        Response.Cookies.Append("autohome_token", "", new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = isHttps,
+            SameSite = isHttps ? SameSiteMode.None : SameSiteMode.Lax,
+            Expires = DateTime.UtcNow.AddDays(-1)
+        });
+
+        return Ok();
+    }
+
+}
